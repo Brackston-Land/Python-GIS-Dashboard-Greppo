6e6a899db4c5ce657f348609bef5f901b4a483d9
Brackston Land <147285744+Brackston-Land@users.noreply.github.com>
2023-10-22 13:53:34 -0500

    Delete vector-demo/scratch.py

--- vector-demo/scratch.py
+++
@@ -1,9 +0,0 @@
-from pandas_geojson import read_geojson
-from tabulate import tabulate
-import geopandas as gpd
-
-
-txDot_Cities = gpd.read_file(filename = "ZipCodes.geojson", driver="GeoJSON")
-test = txDot_Cities[txDot_Cities['name'] == 'Austin']
-
-print(tabulate(test, headers='keys', tablefmt='psql'))
